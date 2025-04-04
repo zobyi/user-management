4f32078f1758ba8fe76647bd28674f97cad85961
Brijesh Singh <brijeshsng171979@gmail.com>
2025-04-04 21:12:27 +0530

    first commit

---
+++ src/main/java/com/cybertech/microservice/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.microservice.controller;
+
+import com.example.microservice.model.User;
+import com.example.microservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
---
+++ src/main/java/com/cybertech/microservice/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.microservice.service;
+
+import com.example.microservice.model.User;
+import com.example.microservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}
