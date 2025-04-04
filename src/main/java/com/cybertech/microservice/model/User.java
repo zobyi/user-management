fb5d77706c973ff21b63458493376d52d58ee33b
Brijesh Singh <brijeshsng171979@gmail.com>
2025-04-04 21:58:33 +0530

    fifth commit

--- src/main/java/com/cybertech/microservice/model/User.java
+++ src/main/java/com/cybertech/microservice/model/User.java
@@ -1,6 +1,11 @@
 package com.cybertech.microservice.model;
 
-import javax.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
 
 @Entity
 @Table(name = "users")
